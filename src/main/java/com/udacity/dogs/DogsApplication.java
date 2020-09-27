07122c039e4fdca2f9bd76bde63ef13d68a60ad1
DemonDaddy22 <guptarohan369@gmail.com>
2020-09-27 19:09:55 +0530

    add spring security and comment out eureka part

--- src/main/java/com/udacity/dogs/DogsApplication.java
+++ src/main/java/com/udacity/dogs/DogsApplication.java
@@ -2,10 +2,10 @@ package com.udacity.dogs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class DogsApplication {
 
 	public static void main(String[] args) {
