de85f123756fc02355b7881d93010183c6d4c3d3
DemonDaddy22 <guptarohan369@gmail.com>
2020-10-04 20:37:21 +0530

    add unit tests

---
+++ src/test/java/com/udacity/dogs/web/DogControllerUnitTest.java
@@ -0,0 +1,45 @@
+package com.udacity.dogs.web;
+
+import com.udacity.dogs.service.DogService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(DogController.class)
+public class DogControllerUnitTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    DogService dogService;
+
+    @Test
+    public void getAllDogs() throws Exception {
+        mockMvc.perform(get("/dogs/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().json("[]"));
+
+        verify(dogService, times(1)).getDogs();
+    }
+
+    @Test
+    public void getDogBreed() throws Exception {
+        mockMvc.perform(get("/dogs/breeds/1"))
+                .andExpect(status().isOk());
+
+        verify(dogService, times(1)).getDogBreedById((long) 1);
+    }
+}
